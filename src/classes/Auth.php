5697da3bd35d48145b527a7b7399af5e0615868e
tofarias <tiago.farias.poa@gmail.com>
2019-07-23 09:37:06 -0300

    Fix removendo verificação de sessão

--- src/classes/Auth.php
+++ src/classes/Auth.php
@@ -9,14 +9,11 @@ class Auth
 {
     public function __construct()
     {
-        
+
     }
+
     public static function check() : Bool
     {
-        if (session_status() !== PHP_SESSION_ACTIVE || session_id() === ""){
-            session_start(); 
-        }        
-
         if( isset($_SESSION['user']) && ($_SESSION['user'] instanceof User)  )
         {
             return true;
@@ -27,9 +24,9 @@ class Auth
             {
                 $email = trim($_POST['email']);
                 $passwd = trim($_POST['passwd']);
-                
+
                 $user = User::where('key', $email)->first();
-                
+
                 if( ($user instanceof User) && password_verify($passwd, $user->password) )
                 {
                     $_SESSION['user'] = $user;
