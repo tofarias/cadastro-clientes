2b108bafc7301e23e99cd0c89876a55c5f67376a
tofarias <tiago.farias.poa@gmail.com>
2019-07-23 13:36:33 -0300

    Add criando container de serviços

--- bootstrap.php
+++ bootstrap.php
@@ -1,32 +1,53 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+   error_reporting(E_ALL);
+   ini_set("display_errors", 1);
 
-include 'vendor/autoload.php';
+   include 'vendor/autoload.php';
 
-use Illuminate\Database\Capsule\Manager as Capsule;
-use \App\Session;
+   use Pimple\Container;
 
-$session = new Session();
 
-$fm = new \Plasticbrain\FlashMessages\FlashMessages();
+   $container = new Container();
 
-$dotenv = \Dotenv\Dotenv::create(__DIR__);
-$dotenv->load();
+   $container['session'] = function($c){
+      return new \App\Session();
+   };
 
-$capsule = new Capsule;
-$capsule->addConnection([
-   "driver"     => getenv('DRIVER'),
-   "host"       => getenv('HOST'),
-   "database"   => getenv('DB_NAME'),
-   "username"   => getenv('DB_USER'),
-   "password"   => getenv('DB_PASSWORD')
-]);
+   $container['fm'] = function($c){
+      return new \Plasticbrain\FlashMessages\FlashMessages();
+   };
 
-//Make this Capsule instance available globally.
-$capsule->setAsGlobal();
+   $container['dotenv'] = function($c){
+      return \Dotenv\Dotenv::create(__DIR__);
+   };
 
-// Setup the Eloquent ORM.
-$capsule->bootEloquent();
-$capsule->bootEloquent();
+   $container['capsule'] = function($c){
+      return new \Illuminate\Database\Capsule\Manager;
+   };
+
+   $container['auth'] = function($c){
+      return new \App\Auth();
+   };
+
+
+   $_session = $container['session'];
+   $_fm      = $container['fm'];
+   $_dotenv  = $container['dotenv']->load();
+   $_capsule = $container['capsule'];
+   $_auth    = $container['auth'];
+
+   $_capsule->addConnection([
+      "driver"     => getenv('DRIVER'),
+      "host"       => getenv('HOST'),
+      "database"   => getenv('DB_NAME'),
+      "username"   => getenv('DB_USER'),
+      "password"   => getenv('DB_PASSWORD')
+   ]);
+
+   //Make this Capsule instance available globally.
+   $_capsule->setAsGlobal();
+
+   // Setup the Eloquent ORM.
+   $_capsule->bootEloquent();
+   $_capsule->bootEloquent();
